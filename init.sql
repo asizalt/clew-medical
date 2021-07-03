213b7b3eec38c530bf75ae19b8d6da2c9883ef79
Asi Zaltzman <asi.zaltzman@imnasol.com>
2021-07-03 11:51:30 +0300

    setup compose

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS product (
+  product_id INT NOT NULL,
+  medication_name varchar(250) NOT NULL,
+  action_name varchar(250) NOT NULL,
+  event_time TIMESTAMP
+  status varchar(250) NOT NULL,
+  updated_at DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  created_at DEFAULT CURRENT_TIMESTAMP
+  PRIMARY KEY (product_id)
+);
