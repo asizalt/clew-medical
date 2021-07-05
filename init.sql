4501d8f5c19bada09e2f0b37b963af268069650a
Asi Zaltzman <asi.zaltzman@imnasol.com>
2021-07-05 18:59:09 +0300

    fix db

--- init.sql
+++ init.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS events (
-  product_id INT NOT NULL,
+  event_id INT NOT NULL,
   medication_name varchar(250) NOT NULL,
   action_name varchar(250) NOT NULL,
   event_time TIMESTAMP,
   status varchar(250) NOT NULL,
-  updated_at DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  created_at DEFAULT CURRENT_TIMESTAMP
-  PRIMARY KEY (product_id)
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  PRIMARY KEY (event_id)
 );
